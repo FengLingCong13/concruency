650732d250a24e9aff86150a83fd406b11b97d14
819163324@qq.com <819163324@qq.com>
2019-07-06 20:53:14 +0800

    Initial commit

---
+++ src/main/java/com/mall/concruency/ConcruencyApplication.java
@@ -0,0 +1,13 @@
+package com.mall.concruency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcruencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcruencyApplication.class, args);
+    }
+
+}
