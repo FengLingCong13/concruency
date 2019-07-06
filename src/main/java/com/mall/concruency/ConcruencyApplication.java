945ccf0717e12e7b4dcc20f3422bda49ca597a45
819163324@qq.com <819163324@qq.com>
2019-07-06 20:54:49 +0800

    修改

--- src/main/java/com/mall/concruency/ConcruencyApplication.java
+++ src/main/java/com/mall/concruency/ConcruencyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ConcruencyApplication {
 
     public static void main(String[] args) {
+        System.out.println(888);
         SpringApplication.run(ConcruencyApplication.class, args);
     }
 
